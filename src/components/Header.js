5e3f2a4a1803d76771d9ae5e1b799acb18544672
Julia Makhlin <tourdefiurst@yandex.com>
2022-03-22 02:13:37 +0200

    мобильная версия без откр меню регистрации

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import mesto_logo from "../images/mesto_logo.svg";
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header(props) {
   const { pathname } = useLocation();
@@ -10,24 +10,24 @@ export default function Header(props) {
     <header className="header page__header">
       <Link to="/">
         <img className="header__logo" src={mesto_logo} alt="логотип Место" />
-       </Link>
-      {
-        loggedIn ? (
-          <React.Fragment>
+      </Link>
+      {loggedIn ? (
+        <React.Fragment>
+          <div className="header__auth">
             <p className="header__email">{email}</p>
             <button className="header__logout" onClick={onSignOut}>
-            Выйти
-          </button>
-          </React.Fragment>
-        ) : (
-          <Link
+              Выйти
+            </button>
+          </div>
+        </React.Fragment>
+      ) : (
+        <Link
           className="header__auth-link"
           to={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}
         >
           {pathname === "/sign-in" ? "Регистрация" : "Войти"}
         </Link>
-        )
-      }
+      )}
     </header>
   );
 }
